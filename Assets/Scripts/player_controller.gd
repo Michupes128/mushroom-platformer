4efb20f4ea34791318acd1575904643c15e488b7
Michupes128 <misa.rott@gmail.com>
2026-03-15 12:40:30 +0100

    player sprite creation

--- Assets/Scripts/player_controller.gd
+++ Assets/Scripts/player_controller.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
+class_name player_controller
+@export var speed = 10
+@export var jump = 10
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -200.0
+var speedMultiplier = 10.0
+var jumpMultiplier = -20.0
 
 
 func _physics_process(delta: float) -> void:
@@ -12,14 +14,14 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump * jumpMultiplier
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed * speedMultiplier
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed * speedMultiplier)
 
 	move_and_slide()
